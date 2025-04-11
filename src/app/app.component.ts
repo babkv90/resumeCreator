fefe3576e1740c4199658e51202e646547ee2fdf
Abinash Kumar <babkv90@gmail.com>
2025-04-11 19:40:02 +0530

    all changes for resume builder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'resumeCreator';
+  logoSVG = `<svg width="160" height="160" viewBox="0 0 160 160" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <!-- Paste SVG code here -->
+ </svg>`;
 }
